e6de4bcecd8f1f7a265ef84b39e77e2d3ff5f555
Iuri <malinoski.iuri@gmail.com>
2020-01-20 14:41:59 +0000

    modularizated the dev list in a component

--- web/src/App.js
+++ web/src/App.js
@@ -5,6 +5,8 @@ import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import DevItem from './component/DevItem';
+
 
 /* Notes: 
 
@@ -143,17 +145,7 @@ function App() {
             <main>
                 <ul>
                     {devs.map( dev =>(
-                        <li key={dev._id} className="dev-item">
-                            <header>
-                                <img src={dev.avatar_url} alt={dev.name}/>
-                                <div className="user-info">
-                                    <strong>{dev.name}</strong>                            
-                                    <span>{dev.techs.join(', ')}</span>                            
-                                </div>                        
-                            </header>
-                            <p>{dev.bio}</p>
-                            <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
-                        </li>    
+                        <DevItem key={dev._id} dev={dev}/>
                     ))}                    
                     
                 </ul>
