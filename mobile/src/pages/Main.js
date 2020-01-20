64ca1d98697ffa5d845b3b84bc21b189823dfddf
Iuri <malinoski.iuri@gmail.com>
2020-01-20 20:03:00 +0000

    finished main view

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({navigation}) {
 
@@ -35,21 +36,34 @@ function Main({navigation}) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{latitude: -22.5300702, longitude: -43.2154067}}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/5756323?s=460&v=4'}} />
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{latitude: -22.5300702, longitude: -43.2154067}}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/5756323?s=460&v=4'}} />
 
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'malinoski' })
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Iuri</Text>
-                        <Text style={styles.devBio}>Soft. Dev.</Text>
-                        <Text style={styles.devTechs}>Java, PHP</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'malinoski' })
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Iuri</Text>
+                            <Text style={styles.devBio}>Soft. Dev.</Text>
+                            <Text style={styles.devTechs}>Java, PHP</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput 
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#9999"
+                    autoCapitalize="words"
+                    autoCorrect={false}/>
+                <TouchableOpacity onPress={ () => {} } style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF"/>
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -82,6 +96,42 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5,
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8e4dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 15
     }
 })
 
