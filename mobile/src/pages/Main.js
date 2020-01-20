c9202bc40b58e2e02ce6d4e498814c97aad11bb4
Iuri <malinoski.iuri@gmail.com>
2020-01-20 19:30:16 +0000

    Created a sample of a user image on map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,14 +1,85 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
 function Main() {
-    return <MapView style={styles.map}/>
+
+    const [ currentRegion, setCurrentRegion ] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition(){
+            const { granted } = await requestPermissionsAsync();
+
+            if(granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,                    
+                });
+                
+                const { latitude, longitude } = coords;
+                
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04, // used to zoom
+                    longitudeDelta: 0.04, // used to zoom
+                });
+            } 
+        }
+
+        loadInitialPosition();
+    }, []); // [] means to execute just one time
+
+    if(!currentRegion) {
+        return null;
+    }
+
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{latitude: -22.5300702, longitude: -43.2154067}}>
+                <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/5756323?s=460&v=4'}} />
+
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Iuri</Text>
+                        <Text style={styles.devBio}>Soft. Dev.</Text>
+                        <Text style={styles.devTechs}>Java, PHP</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+    );
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
+    },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff',
+    },
+
+    callout: {
+        width: 260,        
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5,
     }
 })
 
