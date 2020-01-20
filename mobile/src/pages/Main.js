4f681988f5c250e1dda7736ddc8efdf3ace1cc66
Iuri <malinoski.iuri@gmail.com>
2020-01-20 21:05:35 +0000

    Added search button feature

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -9,8 +9,8 @@ import api from '../services/api'
 function Main({navigation}) {
 
     const [devs, setDevs] = useState([]);
-
     const [ currentRegion, setCurrentRegion ] = useState(null);
+    const [techs, setTechs] = useState('');
 
     useEffect(() => {
         async function loadInitialPosition(){
@@ -42,7 +42,7 @@ function Main({navigation}) {
             params: {
                 latitude,
                 longitude,
-                techs: 'ReactJS'
+                techs
             }
         });
         
@@ -97,7 +97,10 @@ function Main({navigation}) {
                     placeholder="Buscar devs por techs..."
                     placeholderTextColor="#9999"
                     autoCapitalize="words"
-                    autoCorrect={false}/>
+                    autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
+                />
                 <TouchableOpacity onPress={ loadDevs } style={styles.loadButton}>
                     <MaterialIcons name="my-location" size={20} color="#FFF"/>
                 </TouchableOpacity>
